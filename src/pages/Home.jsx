fb73774959566edf2537d320fec829f8c29011a8
Drew-Andersen <andrewtandersen@gmail.com>
2024-10-14 13:02:40 -0400

    Added a signup page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
         <>
             <p className="text-center pt-5">A place to track your meals and goals all in one place.</p>
             <div className="btn-div">
-                <Link to='login'><Button className='btn btn-login'>Login</Button>{' '}</Link>
-                <Link to='signup'><Button className='btn btn-signup'>Sign-up</Button>{' '}</Link>
+                <Link to='login'><Button className='btn btn-login' variant='success'>Login</Button>{' '}</Link>
+                <Link to='signup'><Button className='btn btn-signup' variant="primary">Sign-up</Button>{' '}</Link>
             </div>
         </>
     )
