a23dc5ba3a49e9d989f613074afa15d46919aa94
Drew-Andersen <andrewtandersen@gmail.com>
2024-11-19 12:19:15 -0500

    Added server-side folders/files

--- server/seeds/userSeeds.js
+++ server/seeds/userSeeds.js
@@ -3,5 +3,10 @@
         "name": "John Doe",
         "email": "john@mail.com",
         "password": "john123"
+    },
+    {
+        "name": "Sally May",
+        "email": "sally@mail.com",
+        "password": "sally123"
     }
 ]
