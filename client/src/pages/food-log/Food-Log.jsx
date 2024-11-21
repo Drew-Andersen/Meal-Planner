5a3664e95ab7e5a0abea2e749ec6d5075096f14f
Drew-Andersen <andrewtandersen@gmail.com>
2024-11-21 18:51:35 -0500

    Added a food log and arrows to checnge the date

--- client/src/pages/food-log/Food-Log.jsx
+++ client/src/pages/food-log/Food-Log.jsx
@@ -1,7 +1,45 @@
 import './food-log.css';
+import { useState } from 'react';
 
 export default function FoodLog() {
+    const [date, setDate] = useState(new Date());
+
+    const formatDate = (date) => {
+        const dayOfWeek = date.getDay();
+        const monthOfYear = date.getMonth();
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        return `${days[dayOfWeek]}, ${months[monthOfYear]} ${new Date(date).getDate()}, ${new Date(date).getFullYear()}`
+    }
+
+    const dateInc = () => {
+        const newDate = new Date(date); 
+        newDate.setDate(date.getDate() + 1); 
+        setDate(newDate);
+    }
+
+    const dateDec = () => {
+        const newDate = new Date(date); 
+        newDate.setDate(date.getDate() - 1);
+        setDate(newDate); 
+    }
+
     return (
-        <h1 className='text-center mt-5'>Food Log Page</h1>
+        <>
+            <div className="m-auto cont">
+                <div className='flex'>
+                    <h4 className='font18'>
+                        <strong>Your Food Log For: </strong>
+                        <div className='flex font15'>
+                            <div className="arrow-btn left-arrow p-1 fa-solid fa-arrow-left" onClick={dateDec}></div>
+                            <div className='date-btn px-2 py-1 text-center'>{formatDate(date)}</div>
+                            <div className="arrow-btn right-arrow p-1 fa-solid fa-arrow-right" onClick={dateInc}></div>
+                            <i className="datepicker p-1 fa-solid fa-calendar-days"></i>
+                        </div>
+                    </h4>
+                </div>
+                <div className="horizontalLine"></div>
+            </div>
+        </>
     )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -15,8 +15,12 @@ export default function Home() {
                 <>
                     <p className="text-center pt-5">A place to track your meals and goals all in one place.</p>
                     <div className="btn-div">
-                        <Link to='login'><Button className='btn btn-login' variant='success'>Login</Button>{' '}</Link>
-                        <Link to='signup'><Button className='btn btn-signup' variant="primary">Sign-up</Button>{' '}</Link>
+                        <Link to='login' className='link'>
+                            <Button className='btn btn-login' variant='success'>Login</Button>{' '}
+                        </Link>
+                        <Link to='signup' className='link'>
+                            <Button className='btn btn-signup' variant="primary">Sign-up</Button>{' '}
+                        </Link>
                     </div>
                 </>
             )}
